4cf7b5deb4c48b0b171e4036355e2a448675d9e0
VicroriaS <120009279+VicroriaS@users.noreply.github.com>
2023-05-11 13:12:48 +0300

    Added ahref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="Weather-app">
       
+    This project is created by Viktoriia Sandul
+    <a href="https://github.com/VicroriaS/forecast-react">Open-soursed on Github</a>
+     
     </div>
   );
-}
+  }
 
-export default App;
+
+
