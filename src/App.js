a10561bcb7e7f2747811a3e17369d370cb5951f6
VicroriaS <120009279+VicroriaS@users.noreply.github.com>
2023-05-11 13:21:59 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
     <div className="Weather-app">
-      
-    This project is created by Viktoriia Sandul
-    <a href="https://github.com/VicroriaS/forecast-react">Open-soursed on Github</a>
-     
+      <h1>Forecast-app</h1>
+      <footer>
+      This project is created by Viktoriia Sandul
+      <a href="https://github.com/VicroriaS/forecast-react"> Open-soursed on Github</a>
+    </footer>
     </div>
   );
-  }
-
-
-
+}
