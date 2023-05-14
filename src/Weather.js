e5e845a490a87920885b9e9fa8919aa16b5bb6db
VicroriaS <120009279+VicroriaS@users.noreply.github.com>
2023-05-14 13:51:48 +0300

    Updated search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,41 +1,59 @@
 import React from "react";
+import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row"></div>
-        <div className="col-9">
-          <input
-            type="search"
-            placeholder="Type the city..."
-            className="form-control"
-          />
-        </div>
-        <div className="col-3">
-          <input type="Submit" value="Search" className="btn btn-primary" />
-        </div>
-      </form>
+import React, { useState } from "react";
+import axios from "axios";
 
-      <h1>Odessa</h1>
-      <ul>
-        <li>Wednesday 12:00 </li>
-        <li>Sunny</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <img src="" alt="Sunny" />{" "}
-        </div>
-        10 C
-      </div>
-      <div className="col-6">
-        <div />
+export default function Search() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Type a city"
+        autoFocus={true}
+        onChange={updateCity}
+      />
+      <button type="Submit">Search</button>
+    </form>
+  );
+  if (loaded) {
+    return (
+      <div>
+        {form}
         <ul>
-          <li>Precipitations:15%</li>
-          <li>Humidity:72%</li>
-          <li>Wind:5 km/h</li>
+          <li>Temperature:{Math.round(weather.temperature)}°C</li>
+          <li>Description:{weather.description}</li>
+          <li>Humidity:{weather.humidity}%</li>
+          <li>Wind:{weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
         </ul>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return form;
+  }
 }
